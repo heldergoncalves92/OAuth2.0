d4bd3aafcbd2923168d4b6cafd383f47bbf33d79
Hélder Gonçalves <helder.goncalves@outsystems.com>
2022-06-11 18:14:42 +0100

    Have link to login

--- project.py
+++ project.py
@@ -20,7 +20,7 @@ session = DBSession()
 # Create anti-forgery state token
 @app.route('/login')
 def showLogin():
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
     login_session['state'] = state
     return "The current session state is %s" % login_session['state']
 
